c1f355012d863b55f2e6e4dd1ca339994b1468bf
Ion MORARU <ion.moraru.c@thalesdigital.io>
2018-04-23 11:35:00 +0200

    Init linting

--- README.md
+++ README.md
@@ -50,3 +50,28 @@ WEBPACK++
 
 https://gist.github.com/coryhouse/d611e83e432f3ae65cc46ebb9b599930
 
+## Linting
+
+JSLint
+JSHint
+ESLint
+
+## Decision 1: Config format
+## Decision 2: Which rules
+## Decision 3: Warnings or errors
+## Decision 4: Plugins
+eslint-react-plugin
+
+## Decision 5: Preset
+
+https://gist.github.com/coryhouse/61f866c7174220777899bcfff03dab7f
+https://github.com/dustinspecker/awesome-eslint
+
+
+
+
+
+
+
+
+
--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -4,14 +4,14 @@ import open from 'open'
 import webpack from 'webpack'
 import config from '../webpack.config.dev'
 
+/* eslint-disable no-console*/
 const port = 3009
 const app = express()
 const compiler = webpack(config)
 
-app.use(require('webpack-dev-middleware')(compiler, () => {
-  noInfo: true
-  publicPath: config.output.publicPath
-}))
+app.use(require('webpack-dev-middleware')(compiler, {
+    noInfo: true, publicPath: config.output.publicPath
+  }))
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../src/index.html'))
